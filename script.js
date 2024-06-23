f9b54815c869d4fa9a8cdc4bbf187b37ad6f5867
sanishmdhr96 <mail.sanishmanandhar@gmail.com>
2024-06-23 20:54:20 +0545

    footer designs

--- script.js
+++ script.js
@@ -19,55 +19,55 @@
         </div>
  */
 async function fetchData() {
-    const productListContainer = document.getElementById('product-list')
-    try {
-        console.info('FETCH HANYO')
-        const data = await fetch("https://fakestoreapi.com/products")
-        console.info('FETCH JSON LAGYO')
-        const response = await data.json()
-        console.info('DATA DEKHAYO')
+  const productListContainer = document.getElementById('product-list')
+  try {
+    console.info('FETCH HANYO')
+    const data = await fetch("https://fakestoreapi.com/products")
+    console.info('FETCH JSON LAGYO')
+    const response = await data.json()
+    console.info('DATA DEKHAYO')
 
-        response.forEach(element => {
-            console.log('element', element)
-            const product = document.createElement('div')
-            product.classList.add('category__product--item')
+    response.forEach(element => {
+      console.log('element', element)
+      const product = document.createElement('div')
+      product.classList.add('category__product--item')
 
-            const imgCont = document.createElement('div')
-            imgCont.classList.add('image-container')
+      const imgCont = document.createElement('div')
+      imgCont.classList.add('image-container')
 
-            const img = document.createElement("img")
-            img.setAttribute('src', element.image)
-            img.setAttribute('alt', element.title)
+      const img = document.createElement("img")
+      img.setAttribute('src', element.image)
+      img.setAttribute('alt', element.title)
 
-            imgCont.appendChild(img)
+      imgCont.appendChild(img)
 
-            const productDesc = document.createElement('div')
-            productDesc.classList.add('product-description')
+      const productDesc = document.createElement('div')
+      productDesc.classList.add('product-description')
 
-            const pH2 = document.createElement('h2')
-            pH2.innerText = element.title
+      const pH2 = document.createElement('h2')
+      pH2.innerText = element.title
 
-            const pPara = document.createElement('p');
-            pPara.innerText = element.description
+      const pPara = document.createElement('p');
+      pPara.innerText = element.description
 
-            const pH5 = document.createElement('h5');
-            pH5.innerText = `NRP ${element.price}`
+      const pH5 = document.createElement('h5');
+      pH5.innerText = `NRP ${element.price}`
 
-            productDesc.appendChild(pH2)
-            productDesc.appendChild(pPara)
-            productDesc.appendChild(pH5)
+      productDesc.appendChild(pH2)
+      productDesc.appendChild(pPara)
+      productDesc.appendChild(pH5)
 
-            product.appendChild(imgCont)
-            product.appendChild(productDesc)
+      product.appendChild(imgCont)
+      product.appendChild(productDesc)
 
-            productListContainer.append(product)
-        });
+      productListContainer.append(product)
+    });
 
 
 
-    } catch (e) {
-        console.error(e)
-    }
+  } catch (e) {
+    console.error(e)
+  }
 }
 
 fetchData()
